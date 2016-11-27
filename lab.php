96913a50b0b890a9bedfe8fed026ac4ba38fc367
Илья Лыжин <nerehtaru@yandex.ru>
2016-11-27 15:46:49 +0000

    lab.php

---
+++ lab.php
@@ -0,0 +1,251 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+include 'config.php';
+$dbcnx = mysql_connect($DBlocation,$DBuser,$DBpasswd);
+mysql_set_charset('utf8');
+if (!$dbcnx) // Если дескриптор равен 0 соединение не установлено
+{
+  echo("<P>сервер базы данных не доступен</P>");
+  exit();
+}
+  mysql_select_db($DBname,$dbcnx);
+  
+echo '<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Лабы БД</title>';
+echo ' <form action="lab.php" method="get">
+ <p>Лаба: <input type="text" name="lab" size="3" maxlength="1"/> ---- Пункт: <input type="text" name="p" size="2" maxlength="1" /></p>
+ <p><input type="submit" value="Ответ"/></p>
+</form>';
+ if($_GET['lab']=='2'){
+     if($_GET['p']==1){
+     $zapros ='SELECT delo, n_kamer, COUNT( * ) c
+            FROM client
+            GROUP BY delo, n_kamer
+            HAVING COUNT( * ) >1
+            ORDER BY COUNT( * ) DESC ';
+            
+            echo 'SELECT delo, n_kamer, COUNT( * ) c<br>
+            FROM client<br>
+            GROUP BY delo, n_kamer<br>
+            HAVING COUNT( * ) >1<br>
+            ORDER BY COUNT( * ) DESC ';
+            echo '<hr>';
+            
+            $result=mysql_query($zapros); 
+            //$result - ассоциированный массив, т.е. таблички, у которой есть названия столбцов 
+            //узнаем, сколько в массиве $result строчек 
+            $n=mysql_num_rows($result); 
+            //вывод на страничку в виде таблицы 
+            echo "<table border=1> 
+            <tr><th>Дело</th><th>№ Камеры</th><th>Кол-во обвиняемых</th></tr>"; 
+            //вывод построчно 
+            for($i=0;$i<$n;$i++) 
+             echo  
+            "<tr><td>",mysql_result($result,$i,delo), 
+            "</td><td>",mysql_result($result,$i,n_kamer), 
+            "</td><td>",mysql_result($result,$i,c), 
+            "</td></tr>"; 
+            echo "</table>"; 
+     }
+    if($_GET['p']==2){
+     $zapros ='select   sum(c.oplata) d
+               from client c, dela d
+               where c.delo = d.delo and d.d_end>DATE_SUB(CURDATE(),Interval 1 YEAR)';
+            
+            echo "SELECT   sum(c.oplata) 'doxod'<br>
+                  FROM client c, dela d<br>
+                  WHERE c.delo = d.delo and d.d_end>DATE_SUB(CURDATE(),Interval 1 YEAR)";
+            echo '<hr>';
+            
+            $result=mysql_query($zapros); 
+            //$result - ассоциированный массив, т.е. таблички, у которой есть названия столбцов 
+            //узнаем, сколько в массиве $result строчек 
+            $n=mysql_num_rows($result); 
+            //вывод на страничку в виде таблицы 
+            echo "<table border=1> 
+            <tr><th>Сумма гонарара</th></tr>"; 
+            //вывод построчно 
+            for($i=0;$i<$n;$i++) 
+             echo  
+            "<tr><td>",mysql_result($result,$i,d), 
+            "</td></tr>"; 
+            echo "</table>"; 
+     }
+     if($_GET['p']==3){
+     $zapros ='SELECT c.fio fio, c.delo delo, d.d_start start
+                FROM client c, dela d
+                WHERE c.delo = d.delo
+                ORDER BY d.d_start DESC
+                LIMIT 0, 15';
+            
+            echo "SELECT c.fio 'подзащитный', c.delo '№ дела', d.d_start 'Дело началось в...'<br>
+                    FROM client c, dela d <br>
+                    WHERE c.delo = d.delo <br>
+                    ORDER BY d.d_start DESC ";
+            echo '<hr>';
+            
+            $result=mysql_query($zapros); 
+            //$result - ассоциированный массив, т.е. таблички, у которой есть названия столбцов 
+            //узнаем, сколько в массиве $result строчек 
+            $n=mysql_num_rows($result); 
+            //вывод на страничку в виде таблицы 
+            echo "<table border=1> 
+            <tr><th>ФИО подзащитного</th><th>№ дела</th><th>Дело началось в...</th></tr>"; 
+            //вывод построчно 
+            for($i=0;$i<$n;$i++) 
+             echo  
+            "<tr><td>",mysql_result($result,$i,fio), 
+            "</td><td>",mysql_result($result,$i,delo), 
+            "</td><td>",mysql_result($result,$i,start), 
+            "</td></tr>"; 
+            echo "</table><hr>"; 
+            
+            //2-3-2
+                 $zapros ='SELECT c.fio fio, c.delo delo, d.d_start start
+                        FROM client c, dela d
+                        WHERE c.delo = d.delo AND (TO_DAYS(d.d_start)-TO_DAYS(c.data_r))<18*365
+                        ORDER BY d.d_start DESC 
+                        LIMIT 0, 15';
+
+            echo "SELECT c.fio 'подзащитный', c.delo '№ дела', d.d_start 'Дело началось в...'<br>
+                    FROM client c, dela d<br>
+                    WHERE c.delo = d.delo AND (TO_DAYS(d.d_start)-TO_DAYS(c.data_r))<18*365<br>
+                    ORDER BY d.d_start DESC  ";
+            echo '<hr>';
+            
+            $result=mysql_query($zapros); 
+            //$result - ассоциированный массив, т.е. таблички, у которой есть названия столбцов 
+            //узнаем, сколько в массиве $result строчек 
+            $n=mysql_num_rows($result); 
+            //вывод на страничку в виде таблицы 
+            echo "<table border=1> 
+            <tr><th>ФИО подзащитного</th><th>№ дела</th><th>Дело началось в...</th></tr>"; 
+            //вывод построчно 
+            for($i=0;$i<$n;$i++) 
+             echo  
+            "<tr><td>",mysql_result($result,$i,fio), 
+            "</td><td>",mysql_result($result,$i,delo), 
+            "</td><td>",mysql_result($result,$i,start), 
+            "</td></tr>"; 
+            echo "</table><hr>";
+            
+            //2-3-3
+            $zapros ='SELECT c.fio fio, c.delo delo, st.max_srok srok, d.d_start start
+                            FROM client c, obvinenie o, st_yk st, dela d
+                            WHERE c.n_pasp=o.client and o.st=st.ST and st.max_srok>=10 and c.delo=d.delo
+                            order by d.d_start DESC
+                            LIMIT 0, 15';
+            
+            echo "SELECT c.fio 'Обвиняемый', c.delo 'дело', st.max_srok 'Максимальный срок', d.d_start 'Дата начала'<br>
+                    FROM client c, obvinenie o, st_yk st, dela d<br>
+                    WHERE c.n_pasp=o.client and o.st=st.ST and st.max_srok>=10 and c.delo=d.delo<br>
+                    order by d.d_start DESC ";
+            echo '<hr>';
+            
+            $result=mysql_query($zapros); 
+            //$result - ассоциированный массив, т.е. таблички, у которой есть названия столбцов 
+            //узнаем, сколько в массиве $result строчек 
+            $n=mysql_num_rows($result); 
+            //вывод на страничку в виде таблицы 
+            echo "<table border=1> 
+            <tr><th>Обвиняемый</th><th>№ дела</th><th>Максимальный срок</th><th>дата начала</th></tr>"; 
+            //вывод построчно 
+            for($i=0;$i<$n;$i++) 
+             echo  
+            "<tr><td>",mysql_result($result,$i,fio), 
+            "</td><td>",mysql_result($result,$i,delo), 
+             "</td><td>",mysql_result($result,$i,srok), 
+            "</td><td>",mysql_result($result,$i,start), 
+            "</td></tr>"; 
+            echo "</table>";
+     }
+  
+ }
+
+  if($_GET['lab']=='3'){
+     if($_GET['p']=='1'){
+         $zapros ='SELECT * from l3z1_2';
+            
+            echo "CREATE VIEW  l3z1_2 (FIO,SROK) <br>
+                    AS SELECT c.fio, c.srok<br>
+                    FROM client c<br>
+                    WHERE srok >1<br>
+                    AND result LIKE  '%осужден условно%'; ";
+            echo '<hr>';
+    
+            $result=mysql_query($zapros); 
+            //$result - ассоциированный массив, т.е. таблички, у которой есть названия столбцов 
+            //узнаем, сколько в массиве $result строчек 
+            $n=mysql_num_rows($result); 
+            //вывод на страничку в виде таблицы 
+            echo "<table border=1> 
+            <tr><th>ФИО</th><th>СРОК</th></tr>"; 
+            //вывод построчно 
+            for($i=0;$i<$n;$i++) 
+             echo  
+            "<tr><td>",mysql_result($result,$i,FIO), 
+            "</td><td>",mysql_result($result,$i,SROK), 
+            "</td></tr>"; 
+            echo "</table>";
+     }
+         if($_GET['p']=='2'){
+         $zapros ='SELECT * from l3z2_1 LIMIT 0, 70';
+            
+            echo "CREATE VIEW  l3z2_1 (DELO,FIO,maxSROK,SROKmin) <br>
+                    AS SELECT c.delo, c.fio, SUM( max_srok ) - c.srok, c.srok - SUM( min_srok ) <br>
+                    FROM client c, st_yk st, obvinenie o<br>
+                    WHERE c.n_pasp = o.client AND o.st = st.ST <br>
+                    GROUP BY c.delo, c.n_pasp, c.fio; ";
+            echo '<hr>';
+    
+            $result=mysql_query($zapros); 
+            //$result - ассоциированный массив, т.е. таблички, у которой есть названия столбцов 
+            //узнаем, сколько в массиве $result строчек 
+            $n=mysql_num_rows($result); 
+            //вывод на страничку в виде таблицы 
+            echo "<table border=1> 
+            <tr><th>ДЕЛО</th><th>ФИО</th><th>СРОК МАКС - Результат</th><th>Результат - мин.срок</th></tr>"; 
+            //вывод построчно 
+            for($i=0;$i<$n;$i++) 
+             echo  
+            "<tr><td>",mysql_result($result,$i,DELO), 
+            "</td><td>",mysql_result($result,$i,FIO), 
+            "</td><td>",mysql_result($result,$i,maxSROK), 
+            "</td><td>",mysql_result($result,$i,SROKmin), 
+            "</td></tr>"; 
+            echo "</table>";
+     }
+      if($_GET['p']=='3'){
+         $zapros ='SELECT * from l3z3_1 LIMIT 0, 70';
+            
+            echo "CREATE VIEW  l3z3_1 (DELO,ST)<br>
+                    AS SELECT c.delo, o.st<br>
+                    FROM client c, obvinenie o <br>
+                    WHERE c.n_pasp = o.client
+                     ";
+            echo '<hr>';
+    
+            $result=mysql_query($zapros); 
+            //$result - ассоциированный массив, т.е. таблички, у которой есть названия столбцов 
+            //узнаем, сколько в массиве $result строчек 
+            $n=mysql_num_rows($result); 
+            //вывод на страничку в виде таблицы 
+            echo "<table border=1> 
+            <tr><th>№ ДЕЛА</th><th>№ Статьи</th></tr>"; 
+            //вывод построчно 
+            for($i=0;$i<$n;$i++) 
+             echo  
+            "<tr><td>",mysql_result($result,$i,DELO), 
+            "</td><td>",mysql_result($result,$i,ST), 
+            "</td></tr>"; 
+            echo "</table>";
+     }
+  }
+  if ($_GET['lab'==4]){
+      
+  }
+  
+?>
